05cd0286677832d3e6ddb3665e9b504de284e291
jorgeernestotayruiz <tayocruz1772@gmail.com>
2023-05-03 12:30:21 -0600

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+interface Targeta{
+  titulo: string;
+  subtitulo: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ejercicio1';
+
+
+
+export class AppComponent implements OnInit {
+
+  title = 'Mi primer trabajo en Angular';
+  public ArregloTargeta : Targeta []= []; //variable de tipo arreglo
+
+  ngOnInit(): void {//Es un metodo de tipo void que se inicializa cuando se ejecuta la página}
+    this.ArregloTargeta= [//cargamos datos al arreglo
+      {titulo: 'video 1', subtitulo: 'subtitulo del video',image:"./assets/P1.jpg"},
+      {titulo: 'video 2', subtitulo: 'subtitulo del video',image:"./assets/P2.jpg"},
+      {titulo: 'video 3', subtitulo: 'subtitulo del video',image:"./assets/P3.jpg"},
+      {titulo: 'video 4', subtitulo: 'subtitulo del video',image:"./assets/P4.jpg"},
+      {titulo: 'video 5', subtitulo: 'subtitulo del video',image:"./assets/P5.jpg"},
+    ]
+  } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderbarComponent } from './sliderbar/sliderbar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderbarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
